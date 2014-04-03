9136445e7bc3a181a91a1d071d105938c64654a1
Udbhav <udbhav.sharma15@gmail.com>
2014-04-03 20:58:24 +0530

    Raw Socket Done

--- 2014-3-25/Raw Socket/client.c
+++ 2014-3-25/Raw Socket/client.c
@@ -1,24 +1,36 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/ip.h>
-#include <arpa/inet.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+void print_ip(int ip)
+{
+    unsigned char bytes[4];
+    bytes[0] = ip & 0xFF;
+    bytes[1] = (ip >> 8) & 0xFF;
+    bytes[2] = (ip >> 16) & 0xFF;
+    bytes[3] = (ip >> 24) & 0xFF;	
+    printf("  %d.%d.%d.%d,", bytes[0], bytes[1], bytes[2], bytes[3]);        
+}
 
 int main(int argc,char* argv[])
 {
 	int s;
 	struct sockaddr_in saddr;
 	char packet[1000];
-	int on=1;
-	if ((s = socket(AF_INET, SOCK_RAW, atoi(argv[1]))) < 0) {
+	const int on=1;
+
+	if ((s = socket(AF_INET, SOCK_RAW, atoi(argv[1]))) < 0) 
+	{
 		perror("error:");
 		exit(EXIT_FAILURE);
 	}
 
-	socklen_t *len = (socklen_t *)sizeof(saddr);
-	int fromlen = sizeof(saddr);int len1=sizeof(on);
+	int fromlen = sizeof(saddr);
+
 	if(setsockopt(s,IPPROTO_IP,IP_HDRINCL,&on,sizeof(on))<0)
 	{
 		perror("setsockopt():\n");
@@ -27,30 +39,23 @@ int main(int argc,char* argv[])
 
 	while(1) 
 	{
-		if (recvfrom(s, (char *)&packet, sizeof(packet), 0,
-			(struct sockaddr *)&saddr, &fromlen) < 0)
+		if (recvfrom(s, (char *)&packet, sizeof(packet), 0,(struct sockaddr *)&saddr, &fromlen) < 0)
 			perror("packet receive error:");
+
 		struct iphdr *ip=(struct iphdr *)packet; 
-		unsigned char b;
-		b = (ip->daddr >> 24) & 0xFF;
+
 		if(atoi(argv[1])==ip->protocol)
 			printf("accepted\n");
-		else printf("not accepted\\n");
-		int i = 0;
-		
-		sleep(2);
+		else 
+			printf("not accepted\\n");
 
 		printf("%d,%d,%d,%d",ip->ihl,ip->version,ip->tos,ip->check);
-		
-		char *temp_ip;
-    	temp_ip=inet_ntoa(ip->saddr.sin_addr);
-    	printf("%s\n",temp_ip);
-
-		temp_ip=inet_ntoa(ip->daddr.sin_addr);
-		printf("%s\n",temp_ip);
-
-		printf("%s\n",packet);
-		printf("\n");
+		print_ip(ip->saddr);
+		print_ip(ip->daddr);
+		char *k=packet;
+		k+=20;
+		printf("%s\n",k);
 	}
+	
 	exit(EXIT_SUCCESS);
 }
--- 2014-3-25/Raw Socket/server.c
+++ 2014-3-25/Raw Socket/server.c
@@ -1,26 +1,24 @@
+#include <netdb.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h> //memcpy(),memset()
-#include <netdb.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/tcp.h>
 #include <netinet/ip.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <netinet/in_systm.h>
-#include <arpa/inet.h>
-#include <errno.h>
 
 int main(int argc,char** argv)
 {
 	int sd;
 	const int on =1;
-
 	struct sockaddr_in daddr;
 	char packet[1000];
-
 	struct iphdr *ip = (struct iphdr *)packet;  
 
 	if ((sd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) 
@@ -31,12 +29,15 @@ int main(int argc,char** argv)
 
 
 	daddr.sin_family = AF_INET;
-	daddr.sin_port = 0; /* not needed in SOCK_RAW */
+	daddr.sin_port = 0; 
 	daddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
 	memset(daddr.sin_zero, 0, sizeof(daddr.sin_zero));
-	memset(packet, 'A', 90);  
 
+	char *k=packet;
+	k+=20;
+	strcpy(k,"Udbhav Sharma");
+	
 	packet[999]=0;
 	ip->ihl = 5;
 	ip->version = 4;
@@ -48,7 +49,7 @@ int main(int argc,char** argv)
 	ip->check = 0;		
 	ip->saddr = inet_addr("1.2.3.4");
 	ip->daddr = inet_addr("127.0.0.1");
-	
+
 	if(setsockopt(sd,IPPROTO_IP,IP_HDRINCL,&on,sizeof(on))<0)
 	{
 		perror("setsockopt():\n");
@@ -65,7 +66,6 @@ int main(int argc,char** argv)
 		if (sendto(sd, (char *)packet, sizeof(packet), 0, 
 			(struct sockaddr *)&daddr, (socklen_t)sizeof(daddr)) < 0)
 			perror("packet send error:");
-		sleep(2);
 		count++;
 	}
 	exit(EXIT_SUCCESS);
